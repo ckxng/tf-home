5cfc3f734a88f6aa2e9787c8c0ac5ef816532993
Cameron King <cking@ckxng.com>
2021-11-12 09:47:16 -0600

    removes unused providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,23 +9,3 @@ provider "oci" {
   fingerprint  = var.oci_key_fingerprint
 }
 
-# Configure the Azure provider
-# Azure CLI is recommended locally
-# Service Principal auth is recommended in automation:
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_client_secret
-# $ export ARM_CLIENT_ID="00000000-0000-0000-0000-000000000000"
-# $ export ARM_CLIENT_SECRET="00000000-0000-0000-0000-000000000000"
-# $ export ARM_SUBSCRIPTION_ID="00000000-0000-0000-0000-000000000000"
-# $ export ARM_TENANT_ID="00000000-0000-0000-0000-000000000000"
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
-#provider "azurerm" {
-#  features {}
-#}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-# Configure the AWS Provider
-#provider "aws" {
-#  region     = var.aws_region
-#  access_key = var.aws_access_key
-#  secret_key = var.aws_secret_key
-#}
